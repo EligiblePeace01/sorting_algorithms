e002641316c3d1c1ee721a98b3be8436aec23105
Joseph-Oje <josephoje247@gmail.com>
2023-11-21 13:21:09 -0800

    sorting algorithms codes

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,71 +1,93 @@
 #include "sort.h"
 
-/**
- *swap_nodes - swap current and next node positon
- *@list: cuarrent position of a linked list
- *
- *Return: Nothing it's a void functions
- */
-void swap_nodes(listint_t **list)
-{
-	listint_t *cpy_list, *cmp_node, *save_node;
+size_t listint_len(listint_t *h);
 
-	cpy_list = *list;
-	cmp_node = cpy_list->next;
-	save_node = cpy_list->prev;
-	cpy_list->next = cmp_node->next;
-	cpy_list->prev = cmp_node;
-	if (cmp_node->next != NULL)
-		cmp_node->next->prev = cpy_list;
-	cmp_node->next = cpy_list;
-	cmp_node->prev = save_node;
-	if (save_node != NULL)
-		save_node->next = cmp_node;
-}
 /**
- *cocktail_sort_list - sort double linked list with cocktail sort
- *@list: double linked list to sort
- *
- *Return: Nothing, it's a void function
+ * cocktail_sort_list - sorts a doubly linked list
+ * of integers in ascending order using the Cocktail shaker
+ * sort algorithm
+ * @list: doubly linked list of integers
  */
 void cocktail_sort_list(listint_t **list)
 {
-	int swap, v_act, v_nxt;
-	listint_t *cpy_list;
+	listint_t *f = NULL;
+	listint_t *b = NULL;
+	listint_t *aux = NULL;
+	size_t n, i;
 
-	cpy_list = *list;
-	swap = 0;
-	do {
-		while (cpy_list->next != NULL)
+	if (list == NULL || *list == NULL)
+		return;
+
+	n = listint_len(*list);
+
+	if (n < 2)
+		return;
+
+	b = *list;
+	for (i = 0; i < n; i++)
+	{
+		for (f = b->next; f->next != NULL; f = f->next)
 		{
-			v_act = cpy_list->n;
-			v_nxt = cpy_list->next->n;
-			if (v_act > v_nxt)
+			if (f->prev->n > f->n)
 			{
-				swap_nodes(&cpy_list);
-				swap = 1;
+				if (f->prev->prev != NULL)
+					f->prev->prev->next = f;
+				else
+					*list = f;
+
+				if (f->next != NULL)
+					f->next->prev = f->prev;
+
+				aux = f->prev->prev;
+
+				f->prev->next = f->next;
+				f->prev->prev = f;
+
+				f->next = f->prev;
+				f->prev = aux;
+
+				print_list(*list);
+				f = f->next;
+			}
+		}
+		for (b = f; b->prev != NULL;)
+		{
+			if (b->prev->n > b->n)
+			{
+				if (b->prev->prev != NULL)
+					b->prev->prev->next = b;
+				else
+					*list = b;
+
+				if (b->next != NULL)
+					b->next->prev = b->prev;
+
+				aux = b->prev->prev;
+
+				b->prev->next = b->next;
+				b->prev->prev = b;
+
+				b->next = b->prev;
+				b->prev = aux;
+
 				print_list(*list);
 			}
 			else
-				cpy_list = cpy_list->next;
+				b = b->prev;
 		}
-		if (swap == 0)
-			break;
-		swap = 0;
-		while (cpy_list->prev != NULL)
-		{
-			v_act = cpy_list->n;
-			v_nxt = cpy_list->prev->n;
-			if (v_act < v_nxt)
-			{
-				swap_nodes(&(cpy_list->prev));
-				swap = 1;
-				if (cpy_list->prev == NULL)
-					*list = cpy_list;
-				print_list(*list);
-			}
-			else
-				cpy_list = cpy_list->prev;
-		}
-	} while (swap == 1);
+	}
+}
+
+/**
+ *  listint_len - number of elements in a linked listint_t list.
+ * @h: header of listint_t
+ * Return: the number of nodes
+ */
+size_t listint_len(listint_t *h)
+{
+	size_t len_list;
+
+	for (len_list = 0; h != NULL; len_list++)
+		h = h->next;
+	return (len_list);
 }
