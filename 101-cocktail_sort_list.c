f0167a9bbe48171817c82de02adf3872a8f88dad
EligiblePeace01 <enwerempeace@gmail.com>
2023-11-21 01:59:28 -0800

    function that sorts a doubly linked list of integers

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -13,21 +13,21 @@ void cocktail_sort_list(listint_t **list);
  */
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 {
-listint_t *tmp = (*shaker)->next;
+	listint_t *tmp = (*shaker)->next;
 
-if ((*shaker)->prev != NULL)
-(*shaker)->prev->next = tmp;
-else
-*list = tmp;
-tmp->prev = (*shaker)->prev;
-(*shaker)->next = tmp->next;
-if (tmp->next != NULL)
-tmp->next->prev = *shaker;
-else
-*tail = *shaker;
-(*shaker)->prev = tmp;
-tmp->next = *shaker;
-*shaker = tmp;
+	if ((*shaker)->prev != NULL)
+		(*shaker)->prev->next = tmp;
+	else
+		*list = tmp;
+	tmp->prev = (*shaker)->prev;
+	(*shaker)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *shaker;
+	else
+		*tail = *shaker;
+	(*shaker)->prev = tmp;
+	tmp->next = *shaker;
+	*shaker = tmp;
 }
 
 /**
@@ -39,21 +39,21 @@ tmp->next = *shaker;
  */
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 {
-listint_t *tmp = (*shaker)->prev;
+	listint_t *tmp = (*shaker)->prev;
 
-if ((*shaker)->next != NULL)
-(*shaker)->next->prev = tmp;
-else
-*tail = tmp;
-tmp->next = (*shaker)->next;
-(*shaker)->prev = tmp->prev;
-if (tmp->prev != NULL)
-tmp->prev->next = *shaker;
-else
-*list = *shaker;
-(*shaker)->next = tmp;
-tmp->prev = *shaker;
-*shaker = tmp;
+	if ((*shaker)->next != NULL)
+		(*shaker)->next->prev = tmp;
+	else
+		*tail = tmp;
+	tmp->next = (*shaker)->next;
+	(*shaker)->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *shaker;
+	else
+		*list = *shaker;
+	(*shaker)->next = tmp;
+	tmp->prev = *shaker;
+	*shaker = tmp;
 }
 
 /**
@@ -63,36 +63,36 @@ tmp->prev = *shaker;
  */
 void cocktail_sort_list(listint_t **list)
 {
-listint_t *tail, *shaker;
-bool shaken_not_stirred = false;
+	listint_t *tail, *shaker;
+	bool shaken_not_stirred = false;
 
-if (list == NULL || *list == NULL || (*list)->next == NULL)
-return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-for (tail = *list; tail->next != NULL;)
-tail = tail->next;
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
 
-while (shaken_not_stirred == false)
-{
-shaken_not_stirred = true;
-for (shaker = *list; shaker != tail; shaker = shaker->next)
-{
-if (shaker->n > shaker->next->n)
-{
-swap_node_ahead(list, &tail, &shaker);
-print_list((const listint_t *)*list);
-shaken_not_stirred = false;
-}
-}
-for (shaker = shaker->prev; shaker != *list;
-shaker = shaker->prev)
-{
-if (shaker->n < shaker->prev->n)
-{
-swap_node_behind(list, &tail, &shaker);
-print_list((const listint_t *)*list);
-shaken_not_stirred = false;
-}
-}
-}
+	while (shaken_not_stirred == false)
+	{
+		shaken_not_stirred = true;
+		for (shaker = *list; shaker != tail; shaker = shaker->next)
+		{
+			if (shaker->n > shaker->next->n)
+			{
+				swap_node_ahead(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+		for (shaker = shaker->prev; shaker != *list;
+				shaker = shaker->prev)
+		{
+			if (shaker->n < shaker->prev->n)
+			{
+				swap_node_behind(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+	}
 }
